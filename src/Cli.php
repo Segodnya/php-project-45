1ec021e9723bd736f85711c6666367072681ae4b
kkurentsov <kkurentsov@team.amocrm.com>
2024-08-11 23:37:04 +0300

    feat: Сторонние библиотеки

---
+++ src/Cli.php
@@ -0,0 +1,17 @@
+<?php
+
+// Добавляем неймспейс
+namespace Hexlet\Code\Cli;
+
+// "Импортируем" (как import в js) библиотеки
+use function cli\line;
+use function cli\prompt;
+
+function welcomeUser()
+{
+    line('Welcome to the Brain Game!');
+
+    $name = prompt('May I have your name?');
+
+    line("Hello, %s!", $name);
+}
